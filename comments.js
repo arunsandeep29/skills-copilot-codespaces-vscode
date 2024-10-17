c48174e364ea936a10e9067ed4cf7ddb15109ca8
arunsandeep29 <chadasandu@gmail.com>
2024-10-17 18:59:22 +0000

    Copilot third commit 4

--- comments.js
+++ comments.js
@@ -1,70 +1,14 @@
 // create a web server
 const express = require('express');
 const app = express();
-app.use(express.json());
+const port = 3000;
 
-// create a comments array
-const comments = [
-    { message: 'Hi there!' },
-    { message: 'Hello!' },
-    { message: 'How are you?' },
-    { message: 'Great!' }
-];
+app.use(express.static('public'));
 
-// get all comments
-app.get('/comments', (req, res) => {
-    res.send(comments);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
-
-// get a comment by id
-app.get('/comments/:id', (req, res) => {
-    const comment = comments[req.params.id];
-    if (!comment) {
-        res.status(404).send('Comment not found');
-    } else {
-        res.send(comment);
-    }
-});
-
-// post a new comment
-app.post('/comments', (req, res) => {
-    const comment = {
-        message: req.body.message
-    };
-    comments.push(comment);
-    res.send(comment);
-});
-
-// update a comment by id
-app.put('/comments/:id', (req, res) => {
-    const comment = comments[req.params.id];
-    if (!comment) {
-        res.status(404).send('Comment not found');
-    } else {
-        comment.message = req.body.message;
-        res.send(comment);
-    }
-});
-
-// delete a comment by id
-app.delete('/comments/:id', (req, res) => {
-    const comment = comments[req.params.id];
-    if (!comment) {
-        res.status(404).send('Comment not found');
-    } else {
-        comments.splice(req.params.id, 1);
-        res.send(comment);
-    }
-});
-
-// listen to a port
-app.listen(3000, () => console.log('Server is running...'));
-
-// Run the server by running the following command on the terminal
-// node comments.js
-
-// Open a browser and type the following URL to see all comments
-// http://localhost:3000/comments
-
-// Open a browser and type the following URL to see a comment by id
-// http://localhost:3000/comments/0
